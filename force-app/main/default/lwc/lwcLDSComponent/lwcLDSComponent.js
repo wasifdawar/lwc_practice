a2bca6b46ea80b09d8d1fe197b154155cf7ef72d
Wasif Dawar <m.wasifdawar@gmail.com>
2023-09-13 02:35:52 +0500

    LDS componnet create record and get record

--- force-app/main/default/lwc/lwcLDSComponent/lwcLDSComponent.js
+++ force-app/main/default/lwc/lwcLDSComponent/lwcLDSComponent.js
@@ -1,19 +1,36 @@
-import { LightningElement } from 'lwc';
-//import { wire,api} from 'lwc';
-import { createRecord, getRecord } from 'lightning/uiRecordApi';   
-//import { getRecord } from 'lightning/uiRecordApi'; 
+import { LightningElement,wire,api} from 'lwc';
+import { createRecord, getRecord  } from 'lightning/uiRecordApi';   
+
 
 export default class LwcLDSComponent extends LightningElement {
     accountName;
     accountPhone;
     
-    // @api
-    // recordID;
+    @api
+    recordId;
 
-    // @wire(getRecord,{recordID:'$recordID,fields:fieldArray})
-    // accountRecord;
-    // get accName(){if(this.accountRecord.data){return this.accountRecord.data.fields.Name.value; }}
-    // get accPhone(){if(this.accountRecord.data){return this.accountRecord.data.fields.Name.value; }}   
+    @wire(getRecord,{recordId:'$recordId',fields:fieldArray})
+     accountRecord;
+
+     get isAccountLoaded() 
+     {
+        if(this.accountRecord.data)
+        return true;
+    return false;
+     }    
+     
+     get accName()
+     {
+        if(this.accountRecord.data)
+        {
+            return this.accountRecord.data.fields.Name.value; 
+        }}
+     get accPhone()
+      {
+        if(this.accountRecord.data)
+        {
+            return this.accountRecord.data.fields.Phone.value; 
+        }}   
 
     handleAccountDetails(event)
     {   // this[event.target.name] =  event.target.value;
@@ -30,6 +47,8 @@ export default class LwcLDSComponent extends LightningElement {
     {
         console.log("this.accountName ------>"+this.accountName);
         console.log("this.accountPhone ------>"+this.accountPhone);
+        console.log("this.accountRecord ------>"+accountRecord);
+
         let  fields = 
         {
             'Name': this.accountName,
@@ -42,7 +61,8 @@ export default class LwcLDSComponent extends LightningElement {
         }
 
         createRecord(record).then(x=>{
-            console.log("VAlue ISiSkajkjsa"+x);
+            console.log("VAlue is "+x);
+            this.recordId = x.id;
         }).catch(err=>{console.log(err);})
     }
 }
